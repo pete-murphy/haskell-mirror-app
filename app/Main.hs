7e624b8db949673a962cf44e12433fc11946276e
Peter Murphy <peter.francis.murphy@gmail.com>
2019-03-27 21:26:54 -0400

    Replace deprecated (23363) operator with connect

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import           Control.Monad.IO.Class   (liftIO)
 import           Data.Aeson               (Value, encode, object, (.=))
 import           Data.Aeson.Parser        (json)
 import           Data.ByteString          (ByteString)
-import           Data.Conduit             (($$))
+import           Data.Conduit             (connect)
 import           Data.Conduit.Attoparsec  (sinkParser)
 import           Network.HTTP.Types       (status200, status400)
 import           Network.Wai              (Application, Response, responseLBS)
@@ -27,7 +27,7 @@ main =
 app :: Application
 app req sendResponse =
   handle (sendResponse . invalidJson) $ do
-    value <- sourceRequestBody req $$ sinkParser json
+    value <- sourceRequestBody req `connect` sinkParser json
     newValue <- liftIO $ pure value
     sendResponse $
       responseLBS status200 [("Content-Type", "application/json")] $
@@ -37,7 +37,3 @@ invalidJson :: SomeException -> Response
 invalidJson ex =
   responseLBS status400 [("Content-Type", "application/json")] $
   encode $ object [("message" .= show ex)]
-
--- Application-specific logic would go here.
-modValue :: Value -> IO Value
-modValue = return
